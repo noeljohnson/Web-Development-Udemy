5eab9ce57e9b33d9fca33e3e320d21d62c083d4c
Noel Johnson <noel.johnson6213@gmail.com>
2023-02-22 20:14:14 +0530

    Added github information

---
+++ Appendix 03 Git/github_info.sh
@@ -0,0 +1,13 @@
+## GTG
+
+# following are the steps to make a local repo avaialble remotely in github
+
+# login to your github account
+# create a new repository (add repo name, description etc)
+# get a link for the remote git repo, add it to the local gits remote branch using the following command
+## git remote add <remote-name> <github-link>
+# check if successfull by the following command
+## git remote -v 
+# (should have information about fetch: download and push: upload)
+
+## TYJC
