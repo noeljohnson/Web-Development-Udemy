7ebeeef9cf2a78c03954a6a1f20a9d4af9646cbf
Noel Johnson <noel.johnson6213@gmail.com>
2023-02-22 18:49:11 +0530

    Added reprsentational information for git

---
+++ Appendix 03 Git/git_repr.sh
@@ -0,0 +1,9 @@
+## GTG
+
+#after merging a branch, if you no longer need it, you can safetly delete it
+## git branch -d <branch-name>
+
+# to see the git history in a graphical mode, use the following
+## git log --graph
+
+## TYJC
