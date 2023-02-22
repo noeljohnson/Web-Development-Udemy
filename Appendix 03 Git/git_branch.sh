5c9179bb53f61309e84799b6978ca6b4d9aff715
Noel Johnson <noel.johnson6213@gmail.com>
2023-02-22 18:43:53 +0530

    Added instruction for branching and merging

---
+++ Appendix 03 Git/git_branch.sh
@@ -0,0 +1,17 @@
+## GTG
+
+# to create a new branch use the following command
+## git branch <branch-name>
+
+# then move in to the branch
+## git checkout <branch-name>
+
+# after adding the features, commit it
+
+# then chekcout to the branch you want to merge with
+## git checkout <recive-branch>
+
+# then merge the changes with the branch by running the command
+## git merge <branch-name>
+
+## TYJC
