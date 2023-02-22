c3ff9ced4b836d0654dac061b3bf877a7fb6618d
Noel Johnson <noel.johnson6213@gmail.com>
2023-02-22 17:43:28 +0530

    Continuing git lectures

---
+++ Appendix 01 Shell/basic_shell_commands.sh
@@ -0,0 +1,19 @@
+## GTG
+
+# to move to the parent directory
+cd ../
+
+# to move to child directory
+## cd <child-directory>
+
+# to get the current directory's absolute path
+pwd
+
+# to list all items in the given directory
+ls
+
+#to print the kind of shell used
+echo $0
+
+
+## TYJC
---
+++ Appendix 03 Git/git_info.sh
@@ -0,0 +1,21 @@
+## GTG
+
+# to create a git directory
+## git init
+
+# to change the branch name
+## git branch -m <new-name-for-branch>
+
+# there are 4 types of data structures in git 1:Blob (for files) 2:Tree (for directories) 3:Commit (For history) 4:Anotated Tags
+
+# to know files that are in staging area and files that are only in working directory, run the following command
+## git status
+# the output has two sections, tracked files are those that are in staging area and the untracked files are only present in the working directory
+
+# to add user info : Name, Email
+## git config --global user.name <username>
+## git config --global user.email <user-email>
+## git config --list
+# the last command list all the configurations used
+
+## TYJC
